28309d1d829b07393598387781495bbafe0336ac
Patrick-MarequesV <patrick_1997marques@hotmail.com>
2022-11-05 18:08:31 -0300

    Concluida captação recursiva dos dados das vagas

--- VagasDev/src/main/java/app/app.java
+++ VagasDev/src/main/java/app/app.java
@@ -10,22 +10,54 @@ import java.util.ArrayList;
 public class app {
 
     public static void main(String[] args) {
-        String url = "https://www.vagas.com.br/";
+        String primaryUrl = "https://www.vagas.com.br/vagas-de-";
+
+        String vagaDesejada = "desenvolvedor";
+
+        String url = String.format("%s%s", primaryUrl, vagaDesejada);
 
         crawl(1,url, new ArrayList<String>());
     }
     private static void crawl(int level, String url, ArrayList<String> visited) {
         if(level<=5){
-            Document doc = request(url, visited);
 
-            if(doc != null){
-                for(Element link : doc.select("a[href]")){
-                    String next_link = link.absUrl("href");
-                    if(visited.contains(next_link)== false){
-                        crawl(level++, next_link, visited);
-                    }
-                }
-            }
+            Document doc = request(url, visited);
+            String oddString = "vaga odd ";
+            String evenString = "vaga even ";
+            int contador = 0;
+
+            coletaDadosVaga(doc, oddString, contador);
+            coletaDadosVaga(doc, evenString, contador);
+
+//                    System.out.println(next_link);
+//
+//                    if(visited.contains(next_link)== false){
+//                        crawl(level++, url, visited);
+//                    }
+        }
+    }
+
+    private static void coletaDadosVaga (Document doc, String tipoVaga, int contador) {
+
+        for (Element link : doc.getElementsByClass(tipoVaga)) {
+            String cargo = link.getElementsByClass("link-detalhes-vaga").text();
+            String linkVaga = String.format("https://www.vagas.com.br%s", link.getElementsByClass("link-detalhes-vaga").attr("href"));
+            String empresa = link.getElementsByClass("emprVaga").text();
+            String nivelVaga = link.getElementsByClass("nivelVaga").text();
+            String detalheVaga = link.getElementsByClass("detalhes").text();
+            String localidade = link.getElementsByClass("vaga-local").text();
+            String dataPublicacao = link.getElementsByClass("data-publicacao").text();
+
+
+            System.out.println(cargo);
+            System.out.println(linkVaga);
+            System.out.println(empresa);
+            System.out.println(nivelVaga);
+            System.out.println(detalheVaga);
+            System.out.println(localidade);
+            System.out.println(dataPublicacao);
+
+            contador++;
 
         }
     }
@@ -36,8 +68,8 @@ public class app {
             Document doc = con.get();
 
             if(con.response().statusCode() == 200){
-                System.out.println("Link: " + url );
-                System.out.println(doc.title());
+//                System.out.println("Link: " + url );
+//                System.out.println(doc.title());
                 v.add(url);
 
                 return doc;
