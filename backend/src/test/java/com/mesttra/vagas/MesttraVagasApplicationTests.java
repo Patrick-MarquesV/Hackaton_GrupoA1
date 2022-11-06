ec150362d778f06d9395f234a40a8ac722ecc8c4
unknown <bhigoreduardo@gmail.com>
2022-11-06 12:11:16 -0300

    Created Spring Boot

---
+++ backend/src/test/java/com/mesttra/vagas/MesttraVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesttra.vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesttraVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
